7b532e513c0d54aa11a0efcb21b9ef00710344cd
Antoan Sarafski <antoansarafski@gmail.com>
2023-05-03 16:01:34 +0300

    Commit with Hero class.

---
+++ HeroesRPG/Models/Heroes/Hero.cs
@@ -0,0 +1,32 @@
+﻿using Heroes.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeroesRPG.Models.Heroes
+{
+    public abstract class Hero : IHero
+    {
+        public string Name => throw new NotImplementedException();
+
+        public int Health => throw new NotImplementedException();
+
+        public int Armour => throw new NotImplementedException();
+
+        public IWeapon Weapon => throw new NotImplementedException();
+
+        public bool IsAlive => throw new NotImplementedException();
+
+        public void AddWeapon(IWeapon weapon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TakeDamage(int points)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
