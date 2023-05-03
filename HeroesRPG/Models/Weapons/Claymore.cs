4d06f5088f0c8fce53cc1d5398043425f70bac53
antoansarafski@protonmail.com <antoansarafski@protonmail.com>
2023-05-03 16:24:20 +0300

    Commit with Mace and Claymore types of Weapons.

---
+++ HeroesRPG/Models/Weapons/Claymore.cs
@@ -0,0 +1,17 @@
+﻿using Heroes.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeroesRPG.Models.Weapons
+{
+    public class Claymore : Weapon
+    {
+        public Claymore(string name, int durability) 
+            : base(name, durability)
+        {
+        }
+    }
+}
