ca70c7ac869dfd88b58b4b1d946ec16b11ae27d2
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-05-07 11:26:50 +0300

    Commit with lists for Barbarians and Knights in Map class.

--- HeroesRPG/Models/Heroes/Knight.cs
+++ HeroesRPG/Models/Heroes/Knight.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace HeroesRPG.Models.Heroes
 {
-    internal class Knight
+    public class Knight : Hero
     {
+        public Knight(string name, int health, int armour) 
+            : base(name, health, armour)
+        {
+        }
     }
 }
--- HeroesRPG/Models/Map/Map.cs
+++ HeroesRPG/Models/Map/Map.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Heroes.Models.Contracts;
+using HeroesRPG.Models.Heroes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,46 @@ using System.Threading.Tasks;
 
 namespace HeroesRPG.Models.Map
 {
-    internal class Map
+    public class Map : IMap
     {
+        public string Fight(ICollection<IHero> players)
+        {
+            //var knights = players
+            //    .OfType<Knight>()
+            //    .Where(kn => kn.IsAlive)
+            //    .ToList();
+
+            //var barbarians = players
+            //    .OfType<Barbarian>()
+            //    .Where(b => b.IsAlive)
+            //    .ToList();
+
+            // This is more optimal, cuz we will foreach our players list only once.
+
+            var knights = new List<Knight>();
+            var barbarians = new List<Barbarian>();
+
+            foreach (var player in players) 
+            {
+                if(player.IsAlive)
+                {
+                    if (player.IsAlive)
+                    {
+                        if (player is Knight knight)
+                        {
+                            knights.Add(knight);
+                        }
+                        else if (player is Barbarian barbarian)
+                        {
+                            barbarians.Add(barbarian);
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Invalid player type.");
+                        }
+                    }
+                }
+            }
+        }
     }
 }
