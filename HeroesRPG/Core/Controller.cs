b1f170db6637537a2b8eb24dc9273f68457bf4c2
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-05-10 14:36:08 +0300

    Commit Controller class with private fields for Heroes and Weapons. Added correct constructor.

--- HeroesRPG/Core/Controller.cs
+++ HeroesRPG/Core/Controller.cs
@@ -1,4 +1,7 @@
 ﻿using Heroes.Core.Contracts;
+using Heroes.Models.Contracts;
+using Heroes.Repositories.Contracts;
+using HeroesRPG.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,6 +10,14 @@ namespace Heroes.Core
 {
     public class Controller : IController
     {
+        private readonly IRepository<IHero> heroes;
+        private readonly IRepository<IWeapon> weapons;
+
+        public Controller()
+        {
+            heroes = new HeroRepository();
+            weapons = new WeaponRepository();
+        }
         public string AddWeaponToHero(string weaponName, string heroName)
         {
             throw new NotImplementedException();
