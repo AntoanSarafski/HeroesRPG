62f9a06291cd702398fd9422b93cffebae8be6a9
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-05-10 14:25:43 +0300

    Commit with WeaponRepository with Add and Remove methods.

--- HeroesRPG/Repositories/WeaponRepository.cs
+++ HeroesRPG/Repositories/WeaponRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Heroes.Models.Contracts;
+using Heroes.Repositories.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,32 @@ using System.Threading.Tasks;
 
 namespace HeroesRPG.Repositories
 {
-    internal class WeaponRepository
+    public class WeaponRepository : IRepository<IWeapon>
     {
+        private readonly Dictionary<string, IWeapon> _weapons;
+        public IReadOnlyCollection<IWeapon> Models => _weapons.Values;
+
+        public void Add(IWeapon model)
+        {
+            _weapons.Add(model.Name, model);
+        }
+
+
+        public bool Remove(IWeapon model)
+        {
+            if (_weapons.ContainsKey(model.Name))
+            {
+                _weapons.Remove(model.Name);
+                return true;
+            }
+            return false;
+        }
+
+
+        public IWeapon FindByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
