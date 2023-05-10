358fb07c96869186778d0cbc302af73237669867
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-05-10 14:21:56 +0300

    Commit with HeroRepository class with Add and Remove methods.

--- HeroesRPG/Repositories/HeroRepository.cs
+++ HeroesRPG/Repositories/HeroRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Heroes.Models.Contracts;
+using Heroes.Repositories.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,31 @@ using System.Threading.Tasks;
 
 namespace HeroesRPG.Repositories
 {
-    internal class HeroRepository
+    public class HeroRepository : IRepository<IHero>
     {
+        private readonly Dictionary<string, IHero> _heroes;
+
+        public IReadOnlyCollection<IHero> Models => _heroes.Values;
+
+        public void Add(IHero model)
+            => _heroes.Add(model.Name, model);
+
+        public bool Remove(IHero model)
+        {
+            if (!_heroes.ContainsKey(model.Name)) return false;
+            else
+            {
+                _heroes.Remove(model.Name);
+                return true;
+            }
+        }
+
+
+        public IHero FindByName(string name)
+        {
+            
+        }
+
+   
     }
 }
