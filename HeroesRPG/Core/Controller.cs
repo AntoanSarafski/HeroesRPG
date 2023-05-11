7067750f504dbff224ea64c38b1184e9cf4b1216
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-05-11 10:22:12 +0300

    Commit with CreateHero method in Controller class.

--- HeroesRPG/Core/Controller.cs
+++ HeroesRPG/Core/Controller.cs
@@ -1,6 +1,7 @@
 ﻿using Heroes.Core.Contracts;
 using Heroes.Models.Contracts;
 using Heroes.Repositories.Contracts;
+using HeroesRPG.Models.Heroes;
 using HeroesRPG.Repositories;
 using System;
 using System.Collections.Generic;
@@ -18,15 +19,37 @@ namespace Heroes.Core
             heroes = new HeroRepository();
             weapons = new WeaponRepository();
         }
+
+        public string CreateHero(string type, string name, int health, int armour)
+        {
+            if (heroes.FindByName(name) != null)
+            {
+                throw new InvalidOperationException($"The hero {name} already exists.");
+            }
+
+            IHero hero = type switch
+            {
+                nameof(Barbarian) => new Barbarian(name, health, armour),
+                nameof(Knight) => new Knight(name, health, armour),
+                _ => throw new InvalidOperationException("Invalid hero type.")
+            };
+
+            heroes.Add(hero);
+
+            var heroAlias = type == nameof(Knight)
+                ? $"Sir {hero.Name}"
+                : $"{nameof(Barbarian)} {hero.Name}";
+
+            return $"Successfully added {heroAlias} to the collection.";
+        }
+
+
         public string AddWeaponToHero(string weaponName, string heroName)
         {
             throw new NotImplementedException();
         }
 
-        public string CreateHero(string type, string name, int health, int armour)
-        {
-            throw new NotImplementedException();
-        }
+        
 
         public string CreateWeapon(string type, string name, int durability)
         {
